79924bc5dc5b5668761abc13c77225d2d28a19d8
JustQerq <theqerq@gmail.com>
2025-11-03 05:23:06 +0400

    weather/history GET close to completion

--- app.py
+++ app.py
@@ -1,18 +1,62 @@
 from flask import Flask, render_template, request
 import sqlite3
+from requests_manager import WeatherRequestsManager
 
 
 app = Flask(__name__)
+weather_req_manager = WeatherRequestsManager()
 
 @app.route('/')
 @app.route('/home')
 def index():
     return render_template('index_html')
 
-@app.route('/weather', methods=['GET'])
+@app.route('/weather/history', methods=['GET'])
 def get_weather():
-    con = sqlite3.connect('weather.db')
-    cur = con.cursor()
-    cur.execute('SELECT datetime, city, country, temperature, temperature_feels, humidity, condition, wind FROM weather_data')
-    data = cur.fetchall()
+    conditions_received = []
+    
+    # Field values to filter (e.g. WHERE field = value)
+    fields = ["rowid", "datetime", "city", "country", "temperature", \
+            "temperature_feels", "humidity", "condition"]
+    fields_received = []
+    for field in fields:
+        value = request.args.get(field, None)
+        if value is not None:
+            conditions_received.append(f"{field}={value}")
+    
+    # Greater conditionals (e.g. WHERE field > value)
+    greater = ["rowid_gr", "datetime_gr", "temperature_gr", "temperature_feels_gr", "humidity_gr"]
+    for gr in greater:
+        value = request.args.get(gr, None)
+        if value is not None:
+            conditions_received.append(f"{field}={value}")
+            
+    # Less conditionals (e.g. WHERE field < value)
+    less = ["rowid_ls", "datetime_ls", "temperature_ls", "temperature_feels_ls", "humidity_ls"]
+    for ls in less:
+        value = request.args.get(ls, None)
+        if value is not None:
+            conditions_received.append(f"{field}={value}")
+    
+    sql_condition = ""
+    if len(conditions_received) > 0:
+        sql_condition = " WHERE" + " AND ".join(conditions_received)
+             
+    
+    with sqlite3.connect('weather.db') as con:
+        cur = con.cursor()
+        cur.execute('SELECT * FROM weather_data' + sql_condition)
+        data = cur.fetchall()
+        
+        # no entries found in the database, need to send a request to weather API
+        if len(data) == 0:
+            if "datetime" in conditions_received:
+                data_json = weather_req_manager.get("history", dt=request.args.get("datetime"))
+            elif "datetime_gr":
+                data_json = weather_req_manager.get("history", dt=request.args.get("datetime_gr"))
+            elif "datetime_ls":
+                data_json = weather_req_manager.get("history", dt=request.args.get("datetime_ls"))
+
+            # Transform the received dict into a list of database fields
+    
     return render_template('weather.html', data=data)
---
+++ requests_manager.py
@@ -0,0 +1,42 @@
+import requests
+import json
+
+
+class RequestsManager:
+    def __init__(self, base_url, request_urls=None):
+        self.BASE_URL = base_url
+        if request_urls is None:
+            self.REQUEST_URLs = {}
+        else:
+            self.REQUEST_URLs = request_urls
+    
+    def get(self, request, **kwargs):
+        """Send HTTP GET request with the provided key-value parameter pairs
+
+        Args:
+            request (str): request name, must be stored in self.REQUEST_URLs
+            **kwargs: key-value pairs of request query parameters, values must be strings
+
+        Returns:
+            dict: the received json file converted to a python dictionary
+        """
+        url = self.BASE_URL + self.REQUEST_URLs[request] + "?" + "&".join([f"{k}={v}" for k,v in kwargs.items()])
+        response = requests.get(url).json()
+        return response
+
+
+class WeatherRequestsManager(RequestsManager):
+    def __init__(self, api_key="3909896001fb4262833143125250211"):
+        super().__init__(base_url="http://api.weatherapi.com/v1", request_urls={"current":"/current.json", "history":"/history.json"})
+        self.API_KEY = api_key
+    
+    def get(self, request, **kwargs):
+        url = self.BASE_URL + self.REQUEST_URLs[request] + f"?key={self.API_KEY}&" + "&".join([f"{k}={v}" for k,v in kwargs.items()])
+        response = requests.get(url).json()
+        return response
+
+
+weather_req_manager = WeatherRequestsManager()
+
+response = json.dumps(weather_req_manager.get("history", q="Moscow", dt="2024-12-23"), indent=4)
+print(response)
